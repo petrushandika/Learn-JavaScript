728723a01f26a7b1e0b3b6d680998584a72406d0
petrushandika <petrushandikasinaga@gmail.com>
2024-03-05 15:57:52 +0700

    Objects

---
+++ Objects/js/main.js
@@ -0,0 +1,64 @@
+const product = {
+    basketball: 2095,
+    'delivery-time': 3,
+    comparePrice: function(product1, product2) {
+        console.log('This is functions: ', product1 + ' and ' + product2)
+    }
+}
+
+product['delivery-time'] = '3 Days';
+
+console.log(product.basketball);
+console.log(product['delivery-time']);
+product.comparePrice('item', 'time');
+
+function isSameProduct(item1, item2) {
+    return (
+        item1.name === item2.name && item1.price === item2.price
+    );
+}
+
+const item3 = {
+    name: 'basketball',
+    price: 2095
+}
+
+const item4 = {
+    name: 'baseball',
+    price: 799
+}
+
+const item5 = {
+    name: 'socks',
+    price: 1025
+}
+
+console.log(isSameProduct('socks', 'socks'));
+
+const lowerCase = 'Good Morning';
+console.log(lowerCase.toLowerCase());
+
+const doubletext = 'test';
+console.log(doubletext.repeat(2));
+
+/*
+Calculation
+*/
+
+let calculation = '';
+
+function updateCalculation(value) {
+    calculation += value;
+    console.log(calculation);
+}
+
+function calculate() {
+    if (calculation !== '') {
+        try {
+            const result = eval(calculation);
+            console.log(result);
+        } catch {
+            console.error('Error', error);
+        }
+    }
+}
