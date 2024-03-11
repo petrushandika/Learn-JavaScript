bb57f038ef847e1885240953e65f23773ee50421
petrushandika <petrushandikasinaga@gmail.com>
2024-03-11 16:24:53 +0700

    Array and Loops

--- Arrays-and-Loops/js/main.js
+++ Arrays-and-Loops/js/main.js
@@ -14,16 +14,17 @@ function addTodo() {
 // Exercise 2
 const todoList = [];
 
+
 function renderSomething() {
     let addText = '';
 
     for (let i = 0; i < todoList.length; i ++) {
         const todo = todoList[i];
         const text = `<p>${todo}</p>`;
-        todoListText += text;
+        addText += text;
     }
 
-    console.log(todoListText);
+    console.log(addText);
     document.querySelector('.result-todo').innerHTML = addText;
 }
 
@@ -35,5 +36,66 @@ function addTodoSomething() {
     console.log(todoList);
 
     addSomething.value = '';
+    renderSomething();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderSomething();
+})
+
+// Practice 3
+const todoAny = [{
+    anything: '',
+    dueDate: ''
+}];
+
+function renderAnything() {
+    let addAny = '';
+
+    for (let i = 0; i < todoAny.length; i ++) {
+        const anyTodoObject = todoAny[i];
+        // const name = anyTodoObject.name;
+        // const dueDate = anyTodoObject.dueDate;
+        const { anything, dueDate } = anyTodoObject;
+        const textAny = `
+        <div>${anything}</div>
+        <div>${dueDate}</div>
+        <button onclick="deleteTodo(${i}, 1)">Delete</button>`;
+        addAny += textAny;
+     }
+
+     document.querySelector('.result-any').innerHTML = addAny;
+}
+
+function todoAnything() {
+    const addAnything = document.querySelector('.todo-three');
+    const anything = addAnything.value;
+
+    const dateInput = document.querySelector('.todo-date');
+    const dueDate = dateInput.value;
+
+    todoAny.push({
+        // anything: name,
+        // dueDate: dueDate,
+        anything,
+        dueDate
+    });
+
+    addAnything.value = '';
+    renderAnything();
+}
+
+function deleteTodo(index) {
+    todoAny.splice(index, 1);
+    renderAnything();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderAnything();
+})
+
+
+
+
+
 
